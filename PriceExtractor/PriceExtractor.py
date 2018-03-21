71746ee6ba1bc950d9143db1c8a2e80dd8b4401a
D. Cem Karagöz <dhncem@yandex.com>
2018-03-22 00:30:00 +0300

    Added explanation and comments for some lines

--- PriceExtractor/PriceExtractor.py
+++ PriceExtractor/PriceExtractor.py
@@ -1,16 +1,17 @@
 import urllib.request
 
-opener = urllib.request.FancyURLopener({})
+opener = urllib.request.FancyURLopener({})#creates a fancy url opener to parse the file
 #url = "https://www.hepsiburada.com/dell-gaming-7577-intel-core-i7-7700hq-8gb-1tb-128gb-ssd-gtx1050ti-freedos-15-6-fhd-ips-tasinabilir-bilgisayar-fb70d128f81c-p-HBV000009FDXO"
 url =  input("Please enter the url? ");
-f = opener.open(url)
-content = f.read()
-str1=str(content)
-splitTagOne = "\"originalPriceFormatted\":\"";
-splitTagTwo = "\",\"unitPriceFormatted";
-str1=str1.split(splitTagOne)[1];
+f = opener.open(url) #get the parsed page
+content = f.read() #read the page into a variable
+str1=str(content)   #convert it to str to easier search
+splitTagOne = "\"originalPriceFormatted\":\""; #after digging a little in the file
+splitTagTwo = "\",\"unitPriceFormatted";       #ı found that this is easy combination to extract the price
+#"originalPriceFormatted":" SOMEPRICE ","unitPriceFormatted need to find part in between
+str1=str1.split(splitTagOne)[1];#split in two and take second part
 #print(str1);
 #print(str1.__len__());
-str1=str1.split(splitTagTwo)[0];
+str1=str1.split(splitTagTwo)[0]; #do the same with rest but take the first part
 print(str1);
 #print(str1.__len__());
